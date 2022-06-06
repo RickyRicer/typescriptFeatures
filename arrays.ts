12930a26c8d5e2aabf660f5f270f4f7159f49ff6
Rick Ansay <rick.ansay@gmail.com>
2022-06-05 19:28:24 -0600

    The all-important interface

--- arrays.ts
+++ arrays.ts
@@ -1,17 +1,13 @@
 const carMakers: string[] = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
-const carsByMake = [
-  ['f150'],
-  ['corolla'],
-  ['camaro']
-];
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
 
 // Help with inference when extracting values
 const car = carMakers[0];
 const myCar = carMakers.pop();
 
-// Prevent imcompatible values 
+// Prevent imcompatible values
 carMakers.push(100);
 
 // Help with 'map'
@@ -20,6 +16,6 @@ carMakers.map((car: string): string => {
 });
 
 // Flexible types
-const importantDates: (Date | string) = [new Date()];
+const importantDates: Date | string = [new Date()];
 importantDates.push('2030-10-10');
 importantDates.push(new Date());
