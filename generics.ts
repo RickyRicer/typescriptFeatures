ac61e1a3d6a0ad4fb9918a447a3d49f6d344d776
Rick Ansay <rick.ansay@gmail.com>
2022-06-19 18:13:42 -0600

    Generic constraints

--- generics.ts
+++ generics.ts
@@ -44,4 +44,31 @@ function printAnything<T>(arr: T[]): void {
   }
 }
 
-printAnything(['a', 'b', 'c']);
+printAnything<string>(['a', 'b', 'c']);
+
+// Generic constraints.
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars([new House(), new House()])
+printHousesOrCars([new Car(), new Car()])
