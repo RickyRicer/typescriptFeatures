3f76ac4f7711a33d68c0a993bb3ca100b3da8921
Rick Ansay <rick.ansay@gmail.com>
2022-07-19 22:04:01 -0600

    More fun with decorators

--- decorators.ts
+++ decorators.ts
@@ -1,19 +1,35 @@
+@classDecorator
 class Boat {
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
-    return `This boat's color is ${this.color}`
-  };
+    return `This boat's color is ${this.color}`;
+  }
 
   @logError('Oops, boat was sunk in the ocean')
-  pilot(): void {
-    throw new Error();
-    console.log('swish');
-  };
-};
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
+  }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+  
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
 
 function testDecorator(target: any, key: string) {
-  console.log(target);
   console.log(key);
 }
 
@@ -21,14 +37,14 @@ function logError(errorMessage: string) {
   return function (target: any, key: string, desc: PropertyDescriptor) {
     const method = desc.value;
 
-    desc.value = function() {
-      try { 
+    desc.value = function () {
+      try {
         method();
       } catch (e) {
         console.log(errorMessage);
-      };
+      }
     };
   };
-};
+}
 
-new Boat().pilot
+new Boat().pilot;
