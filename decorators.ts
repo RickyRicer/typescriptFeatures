3236425d56efe42d49188fb7f4e2b223c3664151
Rick Ansay <rick.ansay@gmail.com>
2022-07-19 21:48:24 -0600

    Create propertydescriptor

--- decorators.ts
+++ decorators.ts
@@ -5,13 +5,14 @@ class Boat {
     return `This boat's color is ${this.color}`
   }
 
-  @testDecorator
+  @logError
   pilot(): void {
+    throw new Error();
     console.log('swish');
   }
 }
 
-function testDecorator(target: any, key: string): void {
+function logError(target: any, key: string, desc: PropertyDescriptor) {
   console.log('Target:', target);
   console.log('Key:', key); 
 }
