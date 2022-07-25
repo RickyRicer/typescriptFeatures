c66abc2ad57e1c3c60806660949f5da0191a741c
Rick Ansay <rick.ansay@gmail.com>
2022-07-24 22:44:46 -0600

    Practical metadata

--- metadata.ts
+++ metadata.ts
@@ -1,16 +1,37 @@
 import 'reflect-metadata'
 
-const plane = {
-  color: 'red'
-};
+// const plane = {
+//   color: 'red'
+// };
 
-Reflect.defineMetadata('note', 'hi there', plane);
-Reflect.defineMetadata('height', 10, plane);
+// Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
 
-const note = Reflect.getMetadata('note', plane);
-const height = Reflect.getMetadata('height', plane);
+// const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
 
-console.log(note);
-console.log(height);
+// console.log(note);
+// console.log(height);
 
+@printMetadata
+class Plane {
+  color: string = 'red';
 
+  @markFunction('HI THERE')
+  fly(): void {
+    console.log('vrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  }
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
